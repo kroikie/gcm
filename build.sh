75f4bd35e811dfe4aadb077c0e9d7ee46ce191f5
Arthur Thompson <arthurthompson@google.com>
2016-01-14 12:51:05 -0800

    Fix typo in build.sh again

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tagCount=$(git tag -l --points-at HEAD | wc -l)
-if [$tagCount = 0]; then
+if ["$tagCount" \> "0"]; then
   ant -f ./client-libraries/java/rest-client/build.xml -lib ./client-libraries/java/rest-client/lib stage
 else
   ant -f ./client-libraries/java/rest-client/build.xml -lib ./client-libraries/java/rest-client/lib deploy
