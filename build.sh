0299d919cd805dc39377eeeb519fee3f6415a960
Arthur Thompson <arthurthompson@google.com>
2016-01-14 12:57:58 -0800

    Trial deploy

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 tagCount=$(git tag -l --points-at HEAD | wc -l)
-if ["$tagCount" \> "0"]; then
-  ant -f ./client-libraries/java/rest-client/build.xml -lib ./client-libraries/java/rest-client/lib stage
-else
+if [ "$tagCount" \> "0" ]; then
+  echo "tagged so deploy"
   ant -f ./client-libraries/java/rest-client/build.xml -lib ./client-libraries/java/rest-client/lib deploy
+else
+  echo "not tagged so stage"
+  ant -f ./client-libraries/java/rest-client/build.xml -lib ./client-libraries/java/rest-client/lib stage
 fi
