20b7166a6e80544c935a109a4571901749e44c49
Arthur Thompson <arthurthompson@google.com>
2016-01-14 13:20:54 -0800

    Update tag matching

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tagCount=$(git tag -l --points-at HEAD | wc -l)
-if [ "$tagCount" \> "0" ]; then
+if [ $tagCount -gt 0 ]; then
   echo "tagged so deploy"
   ant -f ./client-libraries/java/rest-client/build.xml -lib ./client-libraries/java/rest-client/lib deploy
 else
