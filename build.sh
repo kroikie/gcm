7ea201f52398c0a140675ddc981cf938ca868d3e
Arthur Thompson <arthurthompson@google.com>
2016-01-15 11:57:42 -0800

    Use TRAVIS_TAG

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tagCount=$(git tag -l --points-at HEAD | wc -l)
+tagCount=$(wc -l $TRAVIS_TAG)
 if [ $tagCount -gt 0 ]; then
   echo "tagged so deploy"
   ant -f ./client-libraries/java/rest-client/build.xml -lib ./client-libraries/java/rest-client/lib deploy
